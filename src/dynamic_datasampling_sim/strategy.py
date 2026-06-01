5256ee752896fa14cb3e8a0e40e13c46a67e01af
Nickc2042 <nickc2042@gmail.com>
2026-06-01 11:48:04 -0500

    Copied directly from Henry's strategy class as currently I don't see the need to edit anything within it. And I've looked it over and have not spotted any bugs

--- src/dynamic_datasampling_sim/strategy.py
+++ src/dynamic_datasampling_sim/strategy.py
@@ -1,1 +1,15 @@
+import numpy as np
 
+
+class strategy:
+    def __init__(self, strattype="Responsive"):
+        self.strattype = strattype
+        self.state = False
+        self.highquality = True
+        self.freqACT = 1
+        self.freqPAS = 1
+        self.freq = 1
+        self.memorycap = 10
+        self.memory = 0
+        self.costs = np.array([1, 2])
+        self.currentcost = 1
